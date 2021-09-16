e6bd5f5ea1081582fb8c50c6cf4a465aa61f0681
Álvaro Freire <alvaro.freirea@udc.es>
2021-09-16 17:50:43 +0200

    StringCount (remake) and start WordCount

--- src/e1/StringCount.java
+++ src/e1/StringCount.java
@@ -16,40 +16,11 @@ public class StringCount {
         int nWords = 0;
 
         boolean isWord = false;
-        int endOfSentence = text.length() - 1;
-        char[] characters = text.toCharArray();
 
         for (int i = 0; i < text.length(); i++) {
 
-            // if the char is a letter, isWord = true.
-            if (Character.isLetter(characters[i]) && i != endOfSentence) {
-                isWord = true;
-
-                // if char isn't a letter and there have been letters before,
-                // counter goes up.
-            } else if (!Character.isLetter(characters[i]) && isWord) {
-                nWords++;
-                isWord = false;
-
-                // the end of the text counter goes up
-            } else if (Character.isLetter(characters[i]) && i == endOfSentence) {
-                nWords++;
-            }
         }
 
         return nWords;
-    }
-
-
-    /**
-     * Counts the number of times the given character appears in the String .
-     * Accented characters are considered different characters .
-     *
-     * @param text String with the characters
-     * @param c    the character to be found
-     * @return Number of times the character appears in the String or zero if null
-     */
-    public static int countChar(String text, char c) {
-        return 0;
     }
 }
