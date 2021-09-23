93d493a5ac69ae44703e901aea0d9cc63084011f
mateo <mateodiaz780@gmail.com>
2021-09-23 17:07:17 +0200

    test countChar

--- test/e1/StringCountTest.java
+++ test/e1/StringCountTest.java
@@ -19,14 +19,17 @@ class StringCountTest {
     @Test
     void countWords() {
         assertEquals(0, StringCount.countWords(null));
+        assertEquals(1, StringCount.countWords("Hello"));
         assertEquals(2, StringCount.countWords("Hello World"));
-        assertEquals(1, StringCount.countWords("Hello World"));
-        assertEquals(2, StringCount.countWords("Hello World"));
+        assertEquals(5, StringCount.countWords("a b c d e "));
     }
 
     @Test
     void countChar() {
-
+        assertEquals(0, StringCount.countChar(null, 'c'));
+        assertEquals(1, StringCount.countChar("xyx", 'y'));
+        assertEquals(2 , StringCount.countChar("platano y manzana", ' '));
+        assertEquals(5 , StringCount.countChar("platano y manzana", 'a'));
     }
 
     @Test
