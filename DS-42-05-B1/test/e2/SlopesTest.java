4988eb55800f400b10c8bf0c7373c78f86b248a0
Administrador <alvaro.freirea@udc.es>
2021-09-30 15:22:47 +0200

    refactoring project (done)

---
+++ DS-42-05-B1/test/e2/SlopesTest.java
@@ -0,0 +1,59 @@
+package e2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SlopesTest {
+    char[][] map = {
+            {'.', '.', '#', '#', '.', '.', '.', '.', '.', '.', '.'},
+            {'#', '.', '.', '.', '#', '.', '.', '.', '#', '.', '.'},
+            {'.', '#', '.', '.', '.', '.', '#', '.', '.', '#', '.'},
+            {'.', '.', '#', '.', '#', '.', '.', '.', '#', '.', '#'},
+            {'.', '#', '.', '.', '.', '#', '#', '.', '.', '#', '.'},
+            {'.', '.', '#', '.', '#', '#', '.', '.', '.', '.', '.'},
+            {'.', '#', '.', '#', '.', '#', '.', '.', '.', '.', '#'},
+            {'.', '#', '.', '.', '.', '.', '.', '.', '.', '.', '#'},
+            {'#', '.', '#', '#', '.', '.', '.', '#', '.', '.', '.'},
+            {'#', '.', '.', '.', '#', '#', '.', '.', '.', '.', '#'},
+            {'.', '#', '.', '.', '#', '.', '.', '.', '#', '.', '#'}
+    };
+
+    char[][] miniMap = {
+            {'.', '.', '#', '.', '#'},
+            {'#', '.', '.', '.', '.'},
+            {'#', '#', '.', '.', '#'},
+            {'#', '#', '.', '.', '.'},
+            {'.', '#', '.', '#', '.'}
+    };
+
+    char[][] nonSquareMap1 = {
+            {'.'},
+            {'#', '.'},
+            {'.', '#', '.'}
+    };
+    char[][] nonSquareMap2 = {
+            {'.', '#', '.'},
+            {'.', '#'},
+            {'#', '.'}
+    };
+    char[][] invalidCharacter = {
+            {'.', '.', '.'},
+            {'#', '#', '.'},
+            {'.', '#', 'a'}
+    };
+
+    @Test
+    void downTheSlope() {
+        assertEquals(0, Slopes.downTheSlope(invalidCharacter, 1, 1));
+        assertEquals(0, Slopes.downTheSlope(nonSquareMap1, 1, 1));
+        assertEquals(0, Slopes.downTheSlope(nonSquareMap2, 1, 1));
+        assertEquals(1, Slopes.downTheSlope(map, 1, 1));
+        assertEquals(1, Slopes.downTheSlope(miniMap, 1, 1));
+    }
+
+    @Test
+    void jumpTheSlope() {
+        assertEquals(0, Slopes.jumpTheSlope(null, 3, 0));
+    }
+}
