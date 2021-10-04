917ef744b68a35c9cdeaa882eb0cb3ff1fc83139
Administrador <alvaro.freirea@udc.es>
2021-10-04 19:20:38 +0200

    tests

---
+++ DS-42-05-B1/test/e3/MelodyTest.java
@@ -0,0 +1,44 @@
+package e3;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MelodyTest {
+
+    @Test
+    void addNote() {
+    }
+
+    @Test
+    void getNote() {
+    }
+
+    @Test
+    void getAccidental() {
+    }
+
+    @Test
+    void getTime() {
+    }
+
+    @Test
+    void size() {
+    }
+
+    @Test
+    void getDuration() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void testHashCode() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
---
+++ DS-42-05-B1/test/e4/CalculatorTest.java
@@ -0,0 +1,24 @@
+package e4;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    void cleanOperations() {
+    }
+
+    @Test
+    void addOperation() {
+    }
+
+    @Test
+    void executeOperations() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
