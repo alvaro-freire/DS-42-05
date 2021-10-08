0b777964e594533901286e0e23930c6d06f73ccb
Álvaro Freire Ares <76497873+alvaro-freire@users.noreply.github.com>
2021-10-08 23:57:21 +0200

    header

--- DS-42-05-B1/src/e2/Slopes.java
+++ DS-42-05-B1/src/e2/Slopes.java
@@ -1,3 +1,12 @@
+/*
+ * TITLE: Software Design
+ * SUBTITLE: exercise 4
+ * AUTHOR 1: Mateo Díaz Allegue LOGIN 1: mateo.diaz
+ * AUTHOR 2: Álvaro Freire Ares LOGIN 2: alvaro.freirea
+ * GROUP: 4.2
+ * DATE: 08 / 10 / 2021
+ */
+
 package e2;
 
 public class Slopes {
