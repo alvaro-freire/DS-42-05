83e1ff8e21d5eb3e3d2ab7bac686d067f6520379
mateo <mateodiaz780@gmail.com>
2021-11-02 21:09:35 +0100

    Colegio: estructura && imprimirRecompensa

--- DS-42-05-B2/src/e1/Colegio.java
+++ DS-42-05-B2/src/e1/Colegio.java
@@ -1,5 +1,57 @@
 package e1;
 
+import javax.print.Doc;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Colegio {
 
+    public String name;
+
+    /* lista de integrantes del Colegio */
+    List<Integrante> IntegrantesList = new ArrayList<>();
+
+    /* constructor */
+    public Colegio(String name) {
+        this.name = name;
+    }
+
+    public void addIntegrante(Integrante integrante) {
+        IntegrantesList.add(integrante);
+    }
+
+    public String imprimirRecompensas() {
+        int recompensaTotal = 0;
+        String string = "";
+
+        for (Integrante i : IntegrantesList) {
+            string = string.concat(i.getNombre() + i.getApellidos());
+            if (i.getClass() == Estudiante.class) {
+                string = string.concat("(Estudiante de " + ((Estudiante) i).casa);
+            } else if (i.getClass() == Fantasma.class) {
+                string = string.concat("(Fantasma de " + ((Fantasma) i).casa);
+            } else if (i.getClass() == Guardabosques.class) {
+                string = string.concat("(Guardabosques ");
+            } else if (i.getClass() == Docente.class) {
+                string = string.concat("(Docente de " + ((Docente) i).asignatura);
+            } else if (i.getClass() == Conserje.class) {
+                string = string.concat("(Conserje ");
+            } else {
+                // error
+            }
+            string = string.concat(", " + i.getDestroyedHorrocruxes() + "horrocruxes): "
+                    + i.recompensa() + "galeones\n");
+            recompensaTotal += i.recompensa();
+        }
+
+        string += "La recompensa total del Colegio " + name + "es de " + recompensaTotal + "galeones\n";
+
+        return string;
+    }
+
+    public String imprimirSalarios() {
+        String string = "";
+
+        return string;
+    }
 }
