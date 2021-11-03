f53b6503077eacbc08a49ad8ffd3e0ab80bb410f
alvaro-freire <alvaro.freirea@udc.es>
2021-11-03 23:18:28 +0100

    corrected format

--- DS-42-05-B2/src/e1/Colegio.java
+++ DS-42-05-B2/src/e1/Colegio.java
@@ -46,11 +46,11 @@ public class Colegio {
             } else if (i.getClass() == Fantasma.class) {
                 string = string.concat("(Fantasma de " + ((Fantasma) i).casa);
             } else if (i.getClass() == Guardabosques.class) {
-                string = string.concat("(Guardabosques ");
+                string = string.concat("(Guardabosques");
             } else if (i.getClass() == Docente.class) {
                 string = string.concat("(Docente de " + ((Docente) i).asignatura);
             } else if (i.getClass() == Conserje.class) {
-                string = string.concat("(Conserje ");
+                string = string.concat("(Conserje");
             } else {
                 // error ***********************
                 throw new IllegalArgumentException();
@@ -71,13 +71,13 @@ public class Colegio {
 
         for (Integrante i : IntegrantesList) {
             if (i.getClass() != Estudiante.class && i.getClass() != Fantasma.class) {
-                string = string.concat(i.getNombre() + i.getApellidos());
+                string = string.concat(i.getNombre() + " " + i.getApellidos());
                 if (i.getClass() == Guardabosques.class) {
-                    string = string.concat("(Guardabosques) ");
+                    string = string.concat("(Guardabosques): ");
                 } else if (i.getClass() == Docente.class) {
-                    string = string.concat("(Docente de " + ((Docente) i).asignatura + ") ");
+                    string = string.concat("(Docente de " + ((Docente) i).asignatura + "): ");
                 } else if (i.getClass() == Conserje.class) {
-                    string = string.concat("(Conserje) ");
+                    string = string.concat("(Conserje): ");
                 }
                 string = string.concat(((Personal) i).salario() + " galeones\n");
                 salarioTotal += ((Personal) i).salario();
