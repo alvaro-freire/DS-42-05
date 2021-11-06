2591fcb77ecfde802bc5b172ed593f76c6602cde
mateo <mateodiaz780@gmail.com>
2021-11-06 01:13:11 +0100

    setters && constructor refactored

--- DS-42-05-B2/src/e2/Anuncio.java
+++ DS-42-05-B2/src/e2/Anuncio.java
@@ -16,32 +16,98 @@ public class Anuncio implements Comparable<Anuncio> {
 
     /* constructor */
     public Anuncio(int numReferencia, int precioBase, int precioPlaza, int numPlazas, int tamano, int cp) {
-            this.numReferencia = numReferencia;
-            this.precioBase = precioBase;
-            this.precioPlaza = precioPlaza;
-            this.numPlazas = numPlazas;
-            this.tamano = tamano;
-            this.cp = cp;
+
+        if (numReferencia < 0 || precioBase < 0 || precioPlaza < 0 || numPlazas < 0 || tamano < 0 || cp < 0) {
+            throw new IllegalArgumentException();
+        }
+
+        this.numReferencia = numReferencia;
+        this.precioBase = precioBase;
+        this.precioPlaza = precioPlaza;
+        this.numPlazas = numPlazas;
+        this.tamano = tamano;
+        this.cp = cp;
+
     }
 
     /* getters y setters */
-    public int getNumReferencia() { return numReferencia; }
-    public void setNumReferencia(int numReferencia) { this.numReferencia = numReferencia; }
+    public int getNumReferencia() {
+        return numReferencia;
+    }
 
-    public int getPrecioBase() { return precioBase; }
-    public void setPrecioBase(int precioBase) { this.precioBase = precioBase;}
+    public void setNumReferencia(int numReferencia) {
 
-    public int getPrecioPlaza() { return precioPlaza; }
-    public void setPrecioPlaza(int precioPlaza) { this.precioPlaza = precioPlaza; }
+        if (numReferencia < 0) {
+            throw new IllegalArgumentException();
+        }
 
-    public int getNumPlazas() { return numPlazas; }
-    public void setNumPlazas(int numPlazas) { this.numPlazas = numPlazas; }
+        this.numReferencia = numReferencia;
+    }
 
-    public int getTamano() { return tamano; }
-    public void setTamano(int tamano) { this.tamano = tamano; }
+    public int getPrecioBase() {
+        return precioBase;
+    }
 
-    public int getCp() { return cp; }
-    public void setCp(int cp) { this.cp = cp; }
+    public void setPrecioBase(int precioBase) {
+
+        if (precioBase < 0) {
+            throw new IllegalArgumentException();
+        }
+
+        this.precioBase = precioBase;
+    }
+
+    public int getPrecioPlaza() {
+        return precioPlaza;
+    }
+
+    public void setPrecioPlaza(int precioPlaza) {
+
+        if (precioPlaza < 0) {
+            throw new IllegalArgumentException();
+        }
+
+        this.precioPlaza = precioPlaza;
+    }
+
+    public int getNumPlazas() {
+        return numPlazas;
+    }
+
+    public void setNumPlazas(int numPlazas) {
+
+        if (numPlazas < 0) {
+            throw new IllegalArgumentException();
+        }
+
+        this.numPlazas = numPlazas;
+    }
+
+    public int getTamano() {
+        return tamano;
+    }
+
+    public void setTamano(int tamano) {
+
+        if (tamano < 0) {
+            throw new IllegalArgumentException();
+        }
+
+        this.tamano = tamano;
+    }
+
+    public int getCp() {
+        return cp;
+    }
+
+    public void setCp(int cp) {
+
+        if (cp < 0) {
+            throw new IllegalArgumentException();
+        }
+
+        this.cp = cp;
+    }
 
     public boolean equals(Object o) {
         if (this == o) return true;
