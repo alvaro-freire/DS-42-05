c50eb5de4d6e42f3a9498477363925c51e671145
alvaro-freire <alvaro.freirea@udc.es>
2021-11-07 13:35:37 +0100

    added equals and getTopicsOfInterest

--- DS-42-05-B2/src/e3/User.java
+++ DS-42-05-B2/src/e3/User.java
@@ -2,20 +2,32 @@ package e3;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class User {
 
-    private final String userName;
+    private final String username;
 
     private final List<TopicOfInterest> topicsOfInterest = new ArrayList<>();
 
-    public User(String userName) {
-        this.userName = userName;
+    public User(String username) {
+        this.username = username;
     }
 
-    public String getUserName() {
-        return userName;
+    public String getUsername() {
+        return username;
     }
 
+    public List<TopicOfInterest> getTopicsOfInterest() {
+        return topicsOfInterest;
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return (Objects.equals(username, user.getUsername()) &&
+                topicsOfInterest == user.getTopicsOfInterest());
+    }
 
 }
