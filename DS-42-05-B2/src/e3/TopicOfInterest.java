9e779b38e3fd6eed6972c13c504e4572bf6a9ec2
mateo <mateodiaz780@gmail.com>
2021-11-08 23:47:32 +0100

    Topics - Number associated

--- DS-42-05-B2/src/e3/TopicOfInterest.java
+++ DS-42-05-B2/src/e3/TopicOfInterest.java
@@ -2,19 +2,19 @@ package e3;
 
 public enum TopicOfInterest {
 
-    Viajes("Viajes"),
-    Deportes("Deportes"),
-    Libros("Libros"),
-    Ropa("Ropa"),
-    Comida("Comida");
+    Viajes(1),
+    Deportes(2),
+    Libros(3),
+    Ropa(4),
+    Comida(5);
 
-    private final String topic;
+    private final int topic;
 
-    TopicOfInterest(String topic) {
+    TopicOfInterest(int topic) {
         this.topic = topic;
     }
 
-    public String getTopic() {
+    public int getTopicNumber() {
         return topic;
     }
 
