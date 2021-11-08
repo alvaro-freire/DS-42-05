33c7646a29be737da21b71949169bbd33ec56f82
mateo <mateodiaz780@gmail.com>
2021-11-08 14:19:07 +0100

    NetworkManager - 2nd implementation (methods implemented)

--- DS-42-05-B2/src/e3/NetworkManager2.java
+++ DS-42-05-B2/src/e3/NetworkManager2.java
@@ -1,41 +1,95 @@
 package e3;
 
-import java.util.List;
+import java.util.*;
 
 public class NetworkManager2 implements NetworkManager {
 
-    @Override
-    public void addUser(String user, List<TopicOfInterest> topicsOfInterest) {
+    Map<String, List<TopicOfInterest>> map = new HashMap<>();
 
+    @Override
+    public void addUser(String username, List<TopicOfInterest> topicsOfInterest) {
+        Iterator<String> iter = map.keySet().iterator();
+
+        if (username == null || topicsOfInterest == null || map.containsKey(username)) {
+            throw new IllegalArgumentException();
+        }
+
+        while(iter.hasNext()){
+            String key = iter.next();
+            if (map.get(key) == topicsOfInterest) {
+                throw new IllegalArgumentException();
+            }
+        }
+
+        map.put(username, topicsOfInterest);
     }
 
     @Override
-    public void removeUser(String user) {
+    public void removeUser(String username) {
 
+        if (username == null) {
+            throw new IllegalArgumentException();
+        }
+
+        if (map.remove(username) == null) {
+            throw new IllegalArgumentException();
+        }
     }
 
     @Override
-    public void addInterest(String user, TopicOfInterest topicOfInterest) {
+    public void addInterest(String username, TopicOfInterest topicOfInterest) {
+        List<TopicOfInterest> list =  map.get(username);
 
+        for (TopicOfInterest topic : list) {
+            if (topic.equals(topicOfInterest)) {
+                throw new IllegalArgumentException();
+            }
+        }
+
+        list.add(topicOfInterest);
     }
 
     @Override
-    public void removeInterest(String user, TopicOfInterest topicOfInterest) {
+    public void removeInterest(String username, TopicOfInterest topicOfInterest) {
+        List<TopicOfInterest> list =  map.get(username);
 
+        for (TopicOfInterest topic : list) {
+            if (topic.equals(topicOfInterest)) {
+                list.remove(topicOfInterest);
+                return;
+            }
+        }
+        throw new IllegalArgumentException();
     }
 
     @Override
     public List<String> getUsers() {
-        return null;
+        return new ArrayList<>(map.keySet());
     }
 
     @Override
     public List<TopicOfInterest> getInterests() {
-        return null;
+        List<TopicOfInterest> list = new ArrayList<>();
+
+        for (String key : map.keySet()) {
+            for (TopicOfInterest topic : map.get(key)) {
+                if (!list.contains(topic)) {
+                    list.add(topic);
+                }
+            }
+        }
+
+        return list;
     }
 
     @Override
-    public List<TopicOfInterest> getInterestsUser(String user) {
-        return null;
+    public List<TopicOfInterest> getInterestsUser(String username) {
+        List<TopicOfInterest> list = map.get(username);
+
+        if (list == null) {
+            throw new IllegalArgumentException();
+        }
+
+        return list;
     }
 }
