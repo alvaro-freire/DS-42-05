c0ea381fd85796b66cec1a8bb58d7ffd18d32e40
mateo <mateodiaz780@gmail.com>
2021-11-09 13:48:49 +0100

    Topic refactored

--- DS-42-05-B2/src/e3/TopicOfInterest.java
+++ DS-42-05-B2/src/e3/TopicOfInterest.java
@@ -2,20 +2,10 @@ package e3;
 
 public enum TopicOfInterest {
 
-    Viajes(1),
-    Deportes(2),
-    Libros(3),
-    Ropa(4),
-    Comida(5);
-
-    private final int topic;
-
-    TopicOfInterest(int topic) {
-        this.topic = topic;
-    }
-
-    public int getTopicNumber() {
-        return topic;
-    }
+    Viajes,
+    Deportes,
+    Libros,
+    Ropa,
+    Comida;
 
 }
